dd6ddc26c4a18e01164bfd1ace6744c6e350936b
vze7 <187817309+vze7@users.noreply.github.com>
2026-09-22 13:00:22 -0300

    fix: load responsive home build

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local Owl = loadstring(game:HttpGet("https://raw.githubusercontent.com/vze7/scripts/main/owl.lua?v=28d5596", true))()
+local Owl = loadstring(game:HttpGet("https://raw.githubusercontent.com/vze7/scripts/main/owl.lua?v=c879c60", true))()
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
