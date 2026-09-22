e6447045a4d36d021f7343793baf311775acaf6d
vze7 <187817309+vze7@users.noreply.github.com>
2026-09-22 13:23:00 -0300

    fix: load stable resize build

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local Owl = loadstring(game:HttpGet("https://raw.githubusercontent.com/vze7/scripts/main/owl.lua?v=1c12cda", true))()
+local Owl = loadstring(game:HttpGet("https://raw.githubusercontent.com/vze7/scripts/main/owl.lua?v=1352853", true))()
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
