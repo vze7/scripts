51266c340da950cd4854abf366a84226b85b0649
vze7 <187817309+vze7@users.noreply.github.com>
2026-09-22 13:07:23 -0300

    fix: load borderless home build

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local Owl = loadstring(game:HttpGet("https://raw.githubusercontent.com/vze7/scripts/main/owl.lua?v=4e3a1e6", true))()
+local Owl = loadstring(game:HttpGet("https://raw.githubusercontent.com/vze7/scripts/main/owl.lua?v=0ccc038", true))()
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
